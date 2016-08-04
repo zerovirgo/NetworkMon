12f10a40258d0b1d77c78c527b6cbf9ae2563365
zerovirgo <zerovirgo@gmail.com>
2016-08-04 13:19:14 +0000

    plot with all day and zoom in plots if needed

--- DailyReport/function_figure_dailyreport.py
+++ DailyReport/function_figure_dailyreport.py
@@ -2,24 +2,26 @@ import os
 import matplotlib
 matplotlib.use('Agg')
 import sys
-sys.path.append('../NetworkDev')
+sys.path.append('../../NetworkDev')
 import function_df
 import pandas as pd
 import pylab as plt
 import numpy as np
 import time
 import timeModule
+from matplotlib.ticker import NullFormatter
+import ana_FlappingBlocks
 
 
 def markduration(infos):
     print 'marking',infos
     plt.text(infos[1],infos[2],'{0}'.format(infos[0]),ha='left',va='bottom',rotation=90,fontsize=10)
 
-def exe(df,bigdf,refTime,filestring,start_sec= 0,end_sec= 0):
+def exe(df,bigdf,refTime,filestring,start_sec= 0,end_sec= 0,number = 86400,subZoom=[]):
     print bigdf.columns
     querystr = 'index > 0'
     additionstr = ''
-    if start_sec > 0.1:
+    if start_sec > 0.1 and end_sec-start_sec < 86200:
         additionstr = '_%.0f_%.0f'%(start_sec-10,end_sec+10)
 
     msg = 'From {0} to {1}\n'.format(time.ctime(start_sec) , time.ctime(end_sec) )
@@ -66,53 +68,60 @@ def exe(df,bigdf,refTime,filestring,start_sec= 0,end_sec= 0):
     for index,item in group.count()['endT'].iteritems():
         msg += '{0}: {1} flappings\n'.format(dicRoute[index],item)
     if start_sec > 0.1:
-        querystr = 'index > {0} and index < {1}'.format(start_sec-10,end_sec+10)
-    #print msg
-    #print 'query and other str = ' , querystr 
-    #print 'addition', additionstr
-    #print 'start_sec = ' , start_sec
-    #print 'total' , len(subdf) , 'togo'
+        querystr = 'index > {0} and index < {1}'.format(start_sec-10,end_sec+100)
     for index ,row in subdf.iterrows():
-        #print 'start',int(row['refTimeStart']),'total ',int(row['refTimeStart'])-int(row['refTimeEnd']) , 'entries'
-        #print 'time from ', time.ctime(row['startT_sec']), 'to', time.ctime(row['endT_sec'])
-        #print 'route=' , row['route'], 'number = ',  dicNumb[row['route']]
-        # in case start point goes below zero
         iStart = int(row['refTimeStart'])
         if row['refTimeStart'] < 0:
             iStart = 0
         df[row['route']].iloc[iStart:int(row['refTimeEnd'])+1] =  dicNumb[row['route']]
-        #for i in range(int(row['refTimeStart']),int(row['refTimeEnd'])):
-        #    df[row['route']][int(i)] = dicNumb[row['route']]
-        #    #df.loc(row['route'],int(i)) = dicNumb[row['route']]
-        #    if i%100 == 0:
-        #        print i,'steps passed'
-
-    #df[['NorthRoute_AMS_CHI_AmsMLXe4','NorthRoute_CHI_AMS_ChiMLXe4','NorthRoute_CHI_TPE_ChiMLXe4','NorthRoute_TPE_CHI_TpeMLXe8']].plot(kind='line',figsize=(12,9))
-    #df.index = pd.date_range(filestring,periods = 86400, freq='s')
     plt.figure(1, figsize=(12,9))
     ax = plt.axes([0.20,0.05,0.78,0.75])
     print 'ctime = {0}'.format(time.ctime(refTime))
-    df['timeIndex'] = pd.date_range(time.ctime(refTime),periods = 86400, freq='s')
+    df['timeIndex'] = pd.date_range(time.ctime(refTime),periods = number, freq='s')
+    timetick0 = pd.date_range(time.ctime(refTime+50+3600), periods = 9 , freq = '3H')
+    timetick = pd.date_range(time.ctime(refTime+50), periods = 9 , freq = '3H')
+    timeticklabel = []
+    print timetick0
+    for item in timetick:
+        print item , type(item)
+        timeticklabel.append('{0}:{1:0>2d}'.format(item.hour,item.minute))
     print 'lendf for draw =' , len(df[['timeIndex','SouthRoute_AMS_CHI_AmsMLXe4','SouthRoute_CHI_AMS_ChiMLXe4','SouthRoute_CHI_TPE_ChiMLXe4','SouthRoute_TPE_CHI_TpeMLXe8']].query(querystr))
     listDFindex = ['timeIndex','SouthRoute_AMS_CHI_AmsMLXe4', 'SouthRoute_CHI_AMS_ChiMLXe4',
                                'SouthRoute_CHI_TPE_ChiMLXe4', 'SouthRoute_TPE_CHI_TpeMLXe8',
                                'NorthRoute_AMS_CHI_AmsMLXe4', 'NorthRoute_CHI_AMS_ChiMLXe4',
                                'NorthRoute_CHI_TPE_ChiMLXe4', 'NorthRoute_TPE_CHI_TpeMLXe8' ]
-    df[listDFindex].query(querystr).plot(kind='line',x = 'timeIndex', ax = ax, marker='o',markersize=1,color='blue',
-            figsize=(12,9),ylim=(0,8.2), alpha = 0.5, legend=False)
+    #ax = df[listDFindex].query(querystr).plot(kind='line',x = 'timeIndex', marker='o',markersize=1,color='blue',
+    if (end_sec-start_sec >= 86200):
+        df[listDFindex].plot(kind='line', marker='o',markersize=1,color='blue',ax = ax,
+                figsize=(12,9),ylim=(0,8.2), alpha = 0.5, legend=False)
+        plt.xticks(np.arange(50,86700,10800),tuple(timeticklabel))
+    else:
+        df[listDFindex].query(querystr).plot(kind='line',x= 'timeIndex', marker='o',markersize=1,color='blue',ax = ax,
+                figsize=(12,9),ylim=(0,8.2), alpha = 0.5, legend=False)
+
     plt.yticks( np.arange(9) , ('','North_CHI2AmsMLXe4','North_AMS2ChiMLXe4','North_TPE2ChiMLXe4','North_CHI2TpeMLXe8',
         'South_CHI2AmsMLXe4','South_AMS2ChiMLXe4','South_TPE2ChiMLXe4','South_CHI2TpeMLXe8'))
     plt.xlabel('Date:{0}'.format(filestring))
     plt.title(msg)
+
     for item in durationXY:
-        plt.text(df['timeIndex'][int(item[1])],item[2],'{0}'.format(item[0]),ha='left',va='bottom',rotation=90,fontsize=10)
+        if (end_sec-start_sec <= 86200):
+            plt.text(df['timeIndex'][int(item[1])],item[2],'{0}'.format(item[0]),ha='left',va='bottom',rotation=90,fontsize=10)
+        else:
+            plt.text(item[1],item[2],'{0}'.format(item[0]),ha='left',va='bottom',rotation=90,fontsize=10)
         #markduration(item)
 
     fpath = os.path.join(os.getcwd(),'Flapping{0}.png'.format(filestring+additionstr))
     plt.savefig(fpath)
+    # leave a copy
+    if (end_sec-start_sec <= 86200):
+        fpath = os.path.join(os.getcwd(),'Flapping{0}.png'.format(filestring+additionstr))
+        plt.savefig(fpath)
     plt.clf()
 
     print fpath
+    if (end_sec-start_sec <= 86200):
+        return
 
     plt.figure(1, figsize=(12,9))
     subdf.hist(column='duration',by='route')
@@ -122,13 +131,9 @@ def exe(df,bigdf,refTime,filestring,start_sec= 0,end_sec= 0):
     plt.savefig(fpath)
     plt.clf()
     return
-    #print df.head(20)
 
 def getDays(seconds):
     import datetime
-    #MM = int(datestring[4:6])
-    #DD = int(datestring[6:])
-    #print MM,DD
     MM = time.gmtime(seconds)[1]
     DD = time.gmtime(seconds)[2]
     print (datetime.datetime(2016,MM,DD)-datetime.datetime(2016,1,1)).days
@@ -139,10 +144,10 @@ def drawDowntimeEventsToday(bigdf):
     yesterday = time.mktime(time.strptime(timeModule.getYesterday(),'%Y%m%d'))
     yesterday_sec = yesterday +28800.
     drawDowntimeEvents(bigdf,yesterday_sec,yesterday_sec+86400)
-    #drawDowntimeEvents(getYesterday())
+
 def drawDowntimeEvents(bigdf,start_sec,end_sec):
     #number = 86400
-    number = 86400
+    number = 86400+3600
     rTest = getDays(start_sec)
     tIndex = np.array(range(0,number))
     NorthRoute_AMS_CHI_AmsMLXe4 = np.zeros(number)
@@ -168,7 +173,7 @@ def drawDowntimeEvents(bigdf,start_sec,end_sec):
     #print df.head()
     datestr = '{0}{1:0>2d}{2:0>2d}'.format( ser[rTest].year , ser[rTest].month , ser[rTest].day)
     print 'datestr = ' , datestr
-    exe(df,bigdf,start_sec-50,datestr,start_sec,end_sec)
+    exe(df,bigdf,start_sec-50,datestr,start_sec,end_sec,number)
 
 #csvlist = [
 #'SouthRoute_CHI_TPE_ChiMLXe4.csv' ,
@@ -186,7 +191,12 @@ for i in csvlist:
 newdf = function_df.getDF_netlog_total( dflist ).sort()
 
 drawDowntimeEventsToday(newdf)
-drawDowntimeEvents(newdf, 1470149625.0,1470150005.0)
+#drawDowntimeEvents(newdf, 1470149625.0,1470150005.0)
+
+intervals = ana_FlappingBlocks.exefunc(newdf)
+for item in intervals:
+    drawDowntimeEvents(newdf,item[0],item[1])
+
 
 #timelist = ana_FlappingBlocks2.exefunc(newdf)
 #for item in timelist:
