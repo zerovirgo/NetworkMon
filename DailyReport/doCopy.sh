e832e67b38fbde7e086ccb3f3cf8e40866d3a5f9
zerovirgo <zerovirgo@gmail.com>
2016-08-29 01:58:50 +0000

    script to conpy log files

--- DailyReport/doCopy.sh
+++ DailyReport/doCopy.sh
@@ -1,8 +1,19 @@
 #!/bin/bash 
 
-filenameBase=`date -d '1 day ago' "+%Y%m%d"`
-filename1="south_route_TPEflapping_${filenameBase}_log"
-filename2="south_route_CHIflapping_${filenameBase}_log"
-tarfile="Flapping_figures_${filenameBase}.tar.gz"
+yday=`date -d '1 day ago' "+%Y%m%d"`
+today=`date -d '' "+%Y%m%d"`
+a=${yday:0:4}
+b=${yday:4:2}
+c=${yday:6:2}
+filename1="south_route_TPEflapping_${yday}_log"
+filename2="south_route_CHIflapping_${yday}_log"
+tarfile="Flapping_figures_${yday}.tar.gz"
 pass=`cat passwd`
-./copyRemote.exp $filename1 $filename2 $pass
+
+/usr/bin/expect copyLogs.exp $a $b $c $pass
+
+scp kschen@117.103.108.26:/root/Routers/Optical/TWBR2_Optical_$yday .
+scp kschen@117.103.108.26:/root/Routers/Optical/CHIBR0_Optical_$yday .
+
+cp CHIBR0_Optical_$yday CHIBR0_Optical_today
+cp TWBR2_Optical_$yday TWBR2_Optical_today
