9f1b7cfdcbc274bf8a6c1f15b03fc06b654e9388
zerovirgo <zerovirgo@gmail.com>
2016-09-30 01:47:59 +0000

    add file

---
+++ DailyReport/doPlot.sh
@@ -0,0 +1,6 @@
+/nfs/home/zero/anaconda2/bin/python ReadLog.py
+/nfs/home/zero/anaconda2/bin/python process_raw_power.py
+/nfs/home/zero/anaconda2/bin/python function_figure_dailyreport.py
+/nfs/home/zero/anaconda2/bin/python function_figure_power.py
+/nfs/home/zero/anaconda2/bin/python anaPower.py
+/nfs/home/zero/anaconda2/bin/python getFlappingPowerTable.py
